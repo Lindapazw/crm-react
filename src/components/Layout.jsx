a008a90d83e4b249a0e60472d1219e386914c32d
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-29 11:38:44 -0300

    add Layout style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,14 @@ import {Outlet} from 'react-router-dom'
 
 const Layout = () => {
     return (
-        <div>
-            <h1 className='text-6xl font-bold'>CRM - React</h1>
-            <Outlet/>
+        <div className='md:flex md:min-h-screen'>            
+            <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
+                <h2 className='text-4xl font-bold text-center text-white'>CRM - Clientes</h2>
+            </aside>
+
+            <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+                <Outlet/>
+            </main>
         </div>
     )
 }
