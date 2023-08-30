967a5598bd1a4218549c1d9cd5834fa0555b32ca
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-30 10:25:10 -0300

    add info cliente in table

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,9 +1,21 @@
 
 function Cliente({cliente}) {
+
+    const {nombre, empresa, email, telefono, id} = cliente;
+    
     return (
-        <tr>
+        <tr className="border-b">
             <td className='p-6'>
-                {cliente.nombre}
+                <p className="text-xl text-gray-800 font-bold">{nombre}</p>
+                <p>{empresa}</p>
+            </td>
+            <td className='p-6'>
+                <p className="text-md text-gray-600 "> <span className="text-gray-800 font-bold">Email: </span>{email}</p>
+                <p className="text-md text-gray-600 "> <span className="text-gray-800 font-bold">Teléfono: </span>{telefono}</p>
+            </td>
+            <td className='p-6 flex gap-3'>
+                <button className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs" type="button">Editar</button>
+                <button className="text-red-600 hover:text-red-700 uppercase font-bold text-xs" type="button">Emilinar</button>
             </td>
         </tr>
     )
