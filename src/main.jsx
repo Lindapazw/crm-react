c03f91ef282d92fc2368d2427b3395de2cffbb9d
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-30 10:59:10 -0300

    add action in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
-import NuevoCliente from './pages/NuevoCliente'
+import NuevoCliente, {action as nuevoClienteAction} from './pages/NuevoCliente'
 import Index, {loader as clientesLoader} from './pages/Index'
 
 const router = createBrowserRouter([
@@ -18,7 +18,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/clientes/nuevo',
-        element: <NuevoCliente/>
+        element: <NuevoCliente/>,
+        action: nuevoClienteAction
       }
     ]
   },
