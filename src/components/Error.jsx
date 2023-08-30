519c4d157db772a17b3a00e15c5a6f9c6b7aa071
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-30 14:17:43 -0300

    fix error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 
 function Error({children}) {
     return (
-        <div className="text-center my-4 bg-red-600 text-white p-3 uppercase">
+        <div className="text-center my-4 bg-red-600 text-white p-3 uppercase rounded-md">
             {children}
         </div>
     )
