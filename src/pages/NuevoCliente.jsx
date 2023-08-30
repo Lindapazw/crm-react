6a2e50303bd7d3fb1da32c10169ff93e1744f486
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-30 10:43:59 -0300

    use navigate

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
 const NuevoCliente = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            <h2>Nuevo cliente</h2>
-        </div>
+        <>
+            <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+            <p className="mt-3">LLena los campos para registrar un nuevo cliente</p>
+
+            <div className="flex justify-end">
+                <button className="bg-blue-800 text-white px-3 py-1 font-bold" onClick={() => navigate('/')}>Volver</button>
+            </div>
+        </>
     )
 }
 
