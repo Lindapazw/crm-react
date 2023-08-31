dd5ef4da93d0d2dec98da6d7f8a1a4f9e5f4e3cd
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-31 15:43:22 -0300

    add action

--- src/pages/EditarClientes.jsx
+++ src/pages/EditarClientes.jsx
@@ -1,6 +1,7 @@
-import { Form, useNavigate, useLoaderData} from "react-router-dom";
-import { obtenerCliente } from "../data/Clientes";
+import { Form, useNavigate, useLoaderData, useActionData, redirect} from "react-router-dom";
+import { obtenerCliente, actualizarCliente } from "../data/Clientes";
 import Formulario from "../components/Formulario";
+import Error from "../components/Error";
 
 export async function loader({params}) {
     const cliente = await obtenerCliente(params.clienteId);
@@ -13,11 +14,37 @@ export async function loader({params}) {
     return cliente;
 }
 
+export async function action({request, params}) {
+    const formData = await request.formData();
+    const datos = Object.fromEntries(formData);
+    const email = formData.get('email');
+    // validacion
+    const errores = []
+    if(Object.values(datos).includes('')) {
+        errores.push('Todos los campos son obligatorios');
+    }
+    // exprecion regular para email
+    let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+    if(!regex.test(email)){
+        errores.push('El email no es válido');
+    }
+    // retorna datos si hay errores
+    if(Object.keys(errores).length) {
+        console.log('Si hay errores');
+        return errores;
+    }
+    // Actualizar el cliente
+    await actualizarCliente(params.clienteId, datos);
+    return redirect('/');
+}
+
+
 function EditarClientes() {
 
     const navigate = useNavigate();
     const cliente = useLoaderData();
-    console.log(cliente);
+    const errores = useActionData();
 
     return (
         <div>
@@ -29,7 +56,7 @@ function EditarClientes() {
             </div>
 
             <div className="bg-white shadow rounded-md md:w-3/4 mx-auto p-5 scroll-py-10 mt-10">
-                {/* {errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )} */}
+                {errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )}
                 <Form method='POST' noValidate>
                     <Formulario cliente={cliente}/>
                     <input className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg" type="submit" value="Guardar cambios"/>
