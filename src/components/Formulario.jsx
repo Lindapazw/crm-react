683e482bddaf94e2ffb48a1d7548da5c55340bf0
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-31 15:27:58 -0300

    add informacion para editar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,6 +12,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Nombre del Cliente"
                     name="nombre"
+                    defaultValue={cliente?.nombre}
                 />
             </div>
             <div className="mb-4">
@@ -25,6 +26,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Empresa del Cliente"
                     name="empresa"
+                    defaultValue={cliente?.empresa}
                 />
             </div>
 
@@ -39,6 +41,8 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Email del Cliente"
                     name="email"
+                    defaultValue={cliente?.email}
+
                 />
             </div>
 
@@ -53,6 +57,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Teléfono del Cliente"
                     name="telefono"
+                    defaultValue={cliente?.telefono}
                 />
             </div>
 
@@ -67,6 +72,7 @@ const Formulario = ({cliente}) => {
                     className="mt-2 block w-full p-3 bg-gray-50 h-20 align-self"
                     placeholder="Notas del Cliente"
                     name="notas"
+                    defaultValue={cliente?.notas}
                 />
             </div>
         </>
