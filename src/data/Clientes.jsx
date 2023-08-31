87f0b0018055f487a122fbd2b4853a538345e270
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-31 10:12:04 -0300

    add agregarCliente

--- src/data/Clientes.jsx
+++ src/data/Clientes.jsx
@@ -3,3 +3,19 @@ export async function obtenerClientes() {
     const resultado = await respuesta.json()
     return resultado;
 }
+
+
+export async function agregarCliente(datos) {
+    try {
+        const respuesta = await fetch(import.meta.env.VITE_API_URL,{
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        await respuesta.json()
+    } catch (error) {
+        console.log(error);
+    }
+}
