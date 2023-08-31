e693340230bf40f9fc0c1c23a61340c9711da89f
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-31 15:27:47 -0300

    add formulario de edicion

--- src/pages/EditarClientes.jsx
+++ src/pages/EditarClientes.jsx
@@ -1,4 +1,6 @@
+import { Form, useNavigate, useLoaderData} from "react-router-dom";
 import { obtenerCliente } from "../data/Clientes";
+import Formulario from "../components/Formulario";
 
 export async function loader({params}) {
     const cliente = await obtenerCliente(params.clienteId);
@@ -12,9 +14,27 @@ export async function loader({params}) {
 }
 
 function EditarClientes() {
+
+    const navigate = useNavigate();
+    const cliente = useLoaderData();
+    console.log(cliente);
+
     return (
         <div>
-            Editar Clientes
+            <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
+            <p className="mt-3">LA continuación podras editar los datos del cliente</p>
+
+            <div className="flex justify-end">
+                <button className="bg-blue-800 text-white px-3 py-1 font-bold" onClick={() => navigate('/')}>Volver</button>
+            </div>
+
+            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto p-5 scroll-py-10 mt-10">
+                {/* {errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )} */}
+                <Form method='POST' noValidate>
+                    <Formulario cliente={cliente}/>
+                    <input className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg" type="submit" value="Guardar cambios"/>
+                </Form>
+            </div>
         </div>
     )
 }
